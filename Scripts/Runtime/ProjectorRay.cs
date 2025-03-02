57c405e582febcbb947c30e23aa63b7782b068cf
t-34400 <34400mt@gmail.com>
2025-03-03 06:49:59 +0900

    Implement the compute shader to update projector ray vertex buffer

---
+++ Scripts/Runtime/ProjectorRay.cs
@@ -0,0 +1,152 @@
+#nullable enable
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace ProjectorRay
+{
+    class ComputeVertexGenerator : MonoBehaviour
+    {
+        [Header("Depth Camera")]
+        [SerializeField] private ComputeShader computeShader = default!;
+        [SerializeField] private int texWidth = 512;
+        [SerializeField] private int texHeight = 512;
+        [SerializeField] private float depthCamNear = 0.1f;
+        [SerializeField] private float depthCamFar = 1f;
+        [SerializeField] private float depthCamFoV = 60f;
+        [SerializeField] private float depthCamAspect = 1.0f; 
+        [SerializeField] private LayerMask layerMask = Physics.DefaultRaycastLayers;
+        [Header("Rendering")]
+        [SerializeField] private Material renderMaterial = default!;
+        [SerializeField] private float depthContinuityThreshold = 0.1f;
+        [SerializeField] private float depthFactor = 0.5f;
+
+
+        private Camera? depthCamera;
+        private RenderTexture? depthTexture;
+        private int kernel;
+        private MeshRenderer? meshRenderer;
+
+        void Start()
+        {
+            meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            meshRenderer.material = renderMaterial;
+            meshRenderer.shadowCastingMode = ShadowCastingMode.Off;
+            meshRenderer.receiveShadows = false;
+
+            var meshFilter = gameObject.AddComponent<MeshFilter>();
+            var mesh = GenerateGridMesh(texWidth, texHeight, 1);
+            meshFilter.mesh = mesh;
+
+            depthCamAspect = (float)texWidth / texHeight;
+            (depthCamera, depthTexture) = CreateDepthCamera(gameObject, texWidth, texHeight, depthCamNear, depthCamFar, depthCamFoV, layerMask);
+
+            kernel = computeShader.FindKernel("CSMain");
+            computeShader.SetTexture(kernel, "_DepthTex", depthTexture);
+
+            mesh.vertexBufferTarget |= GraphicsBuffer.Target.Structured;
+            var vertexBuffer = mesh.GetVertexBuffer(0);
+            computeShader.SetBuffer(kernel, "vertexBuffer", vertexBuffer);
+
+            Debug.Log($"SystemInfo.usesReversedZBuffer: {SystemInfo.usesReversedZBuffer}");
+            computeShader.SetBool("_DepthReversed", SystemInfo.usesReversedZBuffer);
+        }
+
+        void LateUpdate()
+        {
+            if (depthCamera == null || depthTexture == null || meshRenderer == null)
+                return;
+
+            meshRenderer.enabled = false;
+            depthCamera.Render();
+            meshRenderer.enabled = true;
+
+            computeShader.SetInt("_TexWidth", texWidth);
+            computeShader.SetInt("_TexHeight", texHeight);
+            computeShader.SetFloat("_DepthCamAspect", depthCamAspect);
+            computeShader.SetFloat("_DepthContinuityThreshold", depthContinuityThreshold);
+            computeShader.SetFloat("_DepthCamNear", depthCamera.nearClipPlane);
+            computeShader.SetFloat("_DepthCamFar", depthCamera.farClipPlane);
+            computeShader.SetFloat("_DepthCamFoV", depthCamera.fieldOfView * Mathf.Deg2Rad);
+
+            var threadGroupX = Mathf.CeilToInt(texWidth / 8f);
+            var threadGroupY = Mathf.CeilToInt(texHeight / 8f);
+
+            computeShader.Dispatch(kernel, threadGroupX, threadGroupY, 1);
+
+            renderMaterial.SetFloat("_DepthFactor", depthFactor);
+            renderMaterial.SetInt("_TexWidth", texWidth);
+            renderMaterial.SetInt("_TexHeight", texHeight);
+        }
+
+        void OnDestroy()
+        {
+            if (depthTexture != null)
+                depthTexture.Release();
+        }
+
+        static (Camera, RenderTexture) CreateDepthCamera(GameObject gameObject, int texWidth, int texHeight, float near, float far, float fov, LayerMask layerMask)
+        {
+            var depthCamera = gameObject.AddComponent<Camera>();
+
+            depthCamera.nearClipPlane = near;
+            depthCamera.farClipPlane = far;
+            depthCamera.aspect = (float)texWidth / texHeight;
+            depthCamera.enabled = false;
+            depthCamera.depthTextureMode = DepthTextureMode.Depth;
+            depthCamera.cullingMask = layerMask;
+
+            var depthTexture = new RenderTexture(texWidth, texHeight, 24, RenderTextureFormat.Depth);
+            depthTexture.enableRandomWrite = false;
+            depthTexture.Create();
+            depthCamera.targetTexture = depthTexture;
+            depthCamera.fieldOfView = fov;
+
+            return (depthCamera, depthTexture);
+        }
+
+        static Mesh GenerateGridMesh(int width, int height, float size) {
+            var mesh = new Mesh();
+            mesh.indexFormat = IndexFormat.UInt32;
+
+            var vertexCount = width * height;
+            var triangleCount = (width - 1) * (height - 1) * 6;
+
+            var vertices = new Vector3[vertexCount];
+            var uvs = new Vector2[vertexCount];
+            var shaderParams = new Vector2[vertexCount];
+            var triangles = new int[triangleCount];
+
+            for (var z = 0; z < height; z++) {
+                for (var x = 0; x < width; x++) {
+                    var index = x + z * width;
+                    var px = (x / (float)width - 0.5f) * size;
+                    var pz = (z / (float)height - 0.5f) * size;
+                    vertices[index] = new Vector3(px, 0, pz);
+                    uvs[index] = new Vector2(x / (float)(width - 1), z / (float)(height - 1));
+                }
+            }
+
+            int ti = 0;
+            for (var z = 0; z < height - 1; z++) {
+                for (var x = 0; x < width - 1; x++) {
+                    var i = x + z * width;
+                    triangles[ti++] = i;
+                    triangles[ti++] = i + width;
+                    triangles[ti++] = i + 1;
+                    triangles[ti++] = i + 1;
+                    triangles[ti++] = i + width;
+                    triangles[ti++] = i + width + 1;
+                }
+            }
+
+            mesh.vertices = vertices;
+            mesh.uv = uvs;
+            mesh.uv2 = shaderParams;
+            mesh.triangles = triangles;
+            mesh.RecalculateBounds();
+
+            return mesh;
+        }
+    }
+}
--- Shaders/ProjectorRayTransparent.shader
+++ Shaders/ProjectorRayTransparent.shader
@@ -19,17 +19,7 @@ Shader "Custom/ProjectorRayRenderTransparent"
             #pragma fragment frag
             #include "UnityCG.cginc"
 
-            struct VertexData
-            {
-                float3 worldPos;
-                float depth;
-                uint valid;
-            };
-
-            StructuredBuffer<VertexData> _VertexBuffer;
             sampler2D _ProjectionTex;
-            int _TexWidth;
-            int _TexHeight;
             float _DepthFactor;
 
             struct g2f
@@ -41,67 +31,58 @@ Shader "Custom/ProjectorRayRenderTransparent"
 
             struct appdata
             {
-                uint vertexID : SV_VertexID;
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                float2 shaderParams : TEXCOORD1;
             };
 
             struct v2g
             {
-                float2 cellCoord : TEXCOORD0;
+                float4 clipPos : SV_POSITION;
+                float2 uv : TEXCOORD0;
+                float depth : TEXCOORD1;
+                bool valid : TEXCOORD2;
             };
 
             v2g vert(appdata v)
             {
                 v2g o;
-                int cellCountX = _TexWidth - 1;
-                int i = v.vertexID % cellCountX;
-                int j = v.vertexID / cellCountX;
-                o.cellCoord = float2(i, j);
+                o.clipPos = UnityObjectToClipPos(v.vertex);
+
+                o.depth = v.shaderParams.x;
+                o.valid = (v.shaderParams.y > 0.5);
+
+                o.uv = v.uv;
+                
                 return o;
             }
 
-            [maxvertexcount(6)]
-            void geom(point v2g input[1], inout TriangleStream<g2f> triStream)
+            [maxvertexcount(3)]
+            void geom(triangle v2g input[3], inout TriangleStream<g2f> triStream)
             {
-                int i = (int)input[0].cellCoord.x;
-                int j = (int)input[0].cellCoord.y;
-
-                int index00 = j * _TexWidth + i;
-                int index10 = j * _TexWidth + (i + 1);
-                int index01 = (j + 1) * _TexWidth + i;
-                int index11 = (j + 1) * _TexWidth + (i + 1);
-
-                VertexData v00 = _VertexBuffer[index00];
-                VertexData v10 = _VertexBuffer[index10];
-                VertexData v01 = _VertexBuffer[index01];
-                VertexData v11 = _VertexBuffer[index11];
-
-                if(v00.valid == 0 || v10.valid == 0 || v01.valid == 0 || v11.valid == 0)
+                if (!input[0].valid || !input[1].valid || !input[2].valid)
                     return;
 
-                float4 clip00 = UnityWorldToClipPos(v00.worldPos);
-                float4 clip10 = UnityWorldToClipPos(v10.worldPos);
-                float4 clip01 = UnityWorldToClipPos(v01.worldPos);
-                float4 clip11 = UnityWorldToClipPos(v11.worldPos);
-
-                float2 uv00 = float2(i, j) / float2(_TexWidth - 1, _TexHeight - 1);
-                float2 uv10 = float2(i + 1, j) / float2(_TexWidth - 1, _TexHeight - 1);
-                float2 uv01 = float2(i, j + 1) / float2(_TexWidth - 1, _TexHeight - 1);
-                float2 uv11 = float2(i + 1, j + 1) / float2(_TexWidth - 1, _TexHeight - 1);
-
                 g2f o;
-                o.pos = clip00; o.uv = uv00; o.depth = v00.depth; triStream.Append(o);
-                o.pos = clip01; o.uv = uv01; o.depth = v01.depth; triStream.Append(o);
-                o.pos = clip10; o.uv = uv10; o.depth = v10.depth; triStream.Append(o);
+                o.pos = input[0].clipPos;
+                o.uv = input[0].uv;
+                o.depth = input[0].depth;
+                triStream.Append(o);
 
-                o.pos = clip10; o.uv = uv10; o.depth = v10.depth; triStream.Append(o);
-                o.pos = clip11; o.uv = uv11; o.depth = v11.depth; triStream.Append(o);
-                o.pos = clip01; o.uv = uv01; o.depth = v01.depth; triStream.Append(o);
+                o.pos = input[1].clipPos;
+                o.uv = input[1].uv;
+                o.depth = input[1].depth;
+                triStream.Append(o);
+
+                o.pos = input[2].clipPos;
+                o.uv = input[2].uv;
+                o.depth = input[2].depth;
+                triStream.Append(o);
             }
 
             fixed4 frag(g2f i) : SV_Target
             {
                 fixed4 col = tex2D(_ProjectionTex, i.uv);
-
                 col.a *= 1.0 - ((1 - i.depth) * (1 - i.depth) * _DepthFactor);
                 return col;
             }
